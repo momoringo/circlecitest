7ad9eaf9ef4899c425a7743bbc448da904586f81
zukeyama <k.zukeyama@on-sight.ne.jp>
2020-03-29 14:16:16 +0900

    test

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { useState, useCallback } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Rte from 'react-rte';
+
+
+const Btr = React.memo((props) => {
+  console.log(99);
+  return <button onClick={props.onClick}>Click Me</button>
+});
+
+const Wrap = () => {
+  console.log('wrap');
+  return <p>wrap</p>
+}
+
+const Ghj = () => {
+  const [count, setCount] = useState(1);
+  const onClick = useCallback(() => setCount(count => count + 1), []);
+  return (
+
+<>
+    <Btr onClick={onClick}/>
+    <Wrap />
+<p>{count}</p>
+</>
+
+    )
+};
+
+
+function App() {
+    const [rteValue, setRteValue] = useState(Rte.createValueFromString('', 'html'));
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+<Ghj />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+
+
+
+
+export default App;
